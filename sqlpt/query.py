0b442dc3c4d7f6e7f8d2d6896d798a76a53f635e
Bryce Caine <brycecaine@gmail.com>
2020-08-05 23:06:29 -0700

    feat(sqlpt): add concept of Query class
    to tie several other classes together

--- sqlpt/query.py
+++ sqlpt/query.py
@@ -1,3 +1,4 @@
+from sqlparse import parse as sql_parse, sql, tokens as T
 from email import header
 from typing import operator
 
@@ -146,3 +147,88 @@ class WhereClause:
             where_clause_str += str(comparison)
 
         return where_clause_str
+
+
+class Query:
+    def __init__(self, sql_str):
+        self.sql_str = sql_str
+
+    def remove_whitespace(self, token_list):
+        tokens = [x for x in token_list if not x.is_whitespace]
+
+        return tokens
+
+    def tokenize(self):
+        sql_statement = sql_parse(self.sql_str)
+        all_tokens = sql_statement[0].tokens
+        tokens = self.remove_whitespace(all_tokens)
+
+        return tokens
+
+    def from_clause(self):
+        sql_elements = sql_parse(self.sql_str)
+
+        start_appending = False
+        from_clause_tokens = []
+
+        for sql_token in sql_elements[0].tokens:
+            if not sql_token.is_whitespace:
+                if sql_token.value == 'from':
+                    start_appending = True
+
+                if sql_token.value[:5] == 'where':
+                    start_appending = False
+
+                if start_appending:
+                    from_clause_tokens.append(sql_token)
+
+        from_clause_tokens.pop(0)
+        table_name = str(from_clause_tokens.pop(0))
+        first_table = Table(table_name)
+
+        joins = []
+
+        for i, item in enumerate(from_clause_tokens):
+            if str(item) == 'join':
+                left_table = None
+
+                if i == 0:
+                    left_table = first_table
+
+                right_table = Table(str(from_clause_tokens[i+1]))
+                comparison = from_clause_tokens[i+3]
+                join = Join(left_table, right_table, comparison)
+
+                joins.append(join)
+
+        from_clause = FromClause(joins)
+
+        return from_clause
+
+    def where_clause(self):
+        sql_elements = sql_parse(self.sql_str)
+
+        comparisons = []
+
+        for sql_token in sql_elements[0].tokens:
+            if type(sql_token) == sql.Where:
+                where_tokens = sql_token.tokens
+
+                for where_token in where_tokens:
+                    if type(where_token) == sql.Comparison:
+
+                        comparison_tokens = self.remove_whitespace(where_token.tokens)
+                        for i, comparison_token in enumerate(comparison_tokens):
+                            if type(comparison_token) == sql.Identifier:
+                                left_expression = comparison_token.value
+                                operator = comparison_tokens[i+1].value
+                                right_expression = comparison_tokens[i+2].value
+
+                                comparison = Comparison(
+                                    left_expression, operator, right_expression)
+
+                                comparisons.append(comparison)
+
+        where_clause = WhereClause(comparisons)
+
+        return where_clause
--- tests/test_api.py
+++ tests/test_api.py
@@ -2,13 +2,12 @@ import unittest
 
 import sqlparse
 from sqlparse import tokens as T
-from sqlparse.sql import (Comparison, Identifier,
+from sqlparse.sql import (Identifier,
                           Statement, Token,
                           TokenList)
 
-from sqlpt import (api, extract_from_clause,
-                   extract_where_clause, query)
-from sqlpt.api import extract_from_clause
+from sqlpt.api import extract_from_clause, extract_where_clause, tokenize
+from sqlpt.query import Query, Join, Table, FromClause, WhereClause, Field, Comparison
 
 
 class TestApi(unittest.TestCase):
@@ -23,8 +22,8 @@ class TestApi(unittest.TestCase):
 
         whitespace = Token(T.Whitespace, ' ')
 
-        table_1 = query.Table('tbl_stu')
-        table_2 = query.Table('tbl_stu_crs')
+        table_1 = Table('tbl_stu')
+        table_2 = Table('tbl_stu_crs')
 
         token_1 = Token(T.Name, 'tbl_stu.id')
         token_list_1 = TokenList([token_1])
@@ -38,14 +37,13 @@ class TestApi(unittest.TestCase):
         token_list_2 = TokenList([token_2])
         field_2 = Identifier(token_list_2)
 
-        join_comparison = Comparison(
-            [field_1, whitespace, comparison_1, whitespace, field_2])
+        join_comparison = Comparison(field_1, '=', field_2)
 
-        join_1 = query.Join(table_1, table_2, join_comparison)
+        join_1 = Join(table_1, table_2, join_comparison)
 
         joins = [join_1]
 
-        from_clause = query.FromClause(joins)
+        from_clause = FromClause(joins)
 
         from_clause_expected = str(from_clause)
         from_clause_actual = str(extract_from_clause(sql))
@@ -78,7 +76,6 @@ class TestApi(unittest.TestCase):
 
         self.assertEqual(from_clause_1, from_clause_2)
         self.assertEqual(where_clause_1, where_clause_2)
-        print(where_clause_1, where_clause_2)
 
     def test_parse(self):
         sql = ("select id, "
@@ -92,10 +89,100 @@ class TestApi(unittest.TestCase):
                " where stu_sem = '2020-Sp' "
                "   and enrl = 1;")
 
-        sql_tokens = api.tokenize(sql)
+        sql_tokens = tokenize(sql)
 
         # print(dir(sql_tokens[-1]))
         # print(sql_tokens[-1].tokens)
+
+
+class TestQuery(unittest.TestCase):
+    def test_extract(self):
+        sql = ("select id, "
+               "       name "
+               "  from tbl_stu "
+               "  join tbl_stu_crs "
+               "    on tbl_stu.id = tbl_stu_crs.stu_id "
+               " where stu_sem = '2020-Sp' "
+               "   and enrl = 1;")
+
+        whitespace = Token(T.Whitespace, ' ')
+
+        table_1 = Table('tbl_stu')
+        table_2 = Table('tbl_stu_crs')
+
+        token_1 = Token(T.Name, 'tbl_stu.id')
+        token_list_1 = TokenList([token_1])
+        field_1 = Identifier(token_list_1)
+
+        comparison_token = Token(T.Operator, '=')
+        comparison_list = TokenList([comparison_token])
+        comparison_1 = Identifier(comparison_list)
+
+        token_2 = Token(T.Name, 'tbl_stu_crs.stu_id')
+        token_list_2 = TokenList([token_2])
+        field_2 = Identifier(token_list_2)
+
+        join_comparison = Comparison(field_1, '=', field_2)
+
+        join_1 = Join(table_1, table_2, join_comparison)
+
+        joins = [join_1]
+
+        from_clause = FromClause(joins)
+
+        from_clause_expected = str(from_clause)
+        query = Query(sql)
+        from_clause_actual = str(query.from_clause())
+
+        self.assertEqual(from_clause_actual, from_clause_expected)
+
+    def test_compare_sql(self):
+        sql_1 = ("select id, "
+                 "       name "
+                 "  from tbl_stu "
+                 "  join tbl_stu_crs "
+                 "    on tbl_stu.id = tbl_stu_crs.stu_id "
+                 " where stu_sem = '2020-Sp' "
+                 "   and enrl = 1;")
+
+        sql_2 = ("select id, "
+                 "       name, "
+                 "       major "
+                 "  from tbl_stu_crs "
+                 "  join tbl_stu "
+                 "    on tbl_stu.id = tbl_stu_crs.stu_id "
+                 " where stu_sem = '2020-Sp' "
+                 "   and enrl = 1;")
+
+        query_1 = Query(sql_1)
+        query_2 = Query(sql_2)
+
+        from_clause_1 = query_1.from_clause()
+        from_clause_2 = query_2.from_clause()
+
+        where_clause_1 = query_1.where_clause()
+        where_clause_2 = query_2.where_clause()
+
+        self.assertEqual(from_clause_1, from_clause_2)
+        self.assertEqual(where_clause_1, where_clause_2)
+
+    def test_parse(self):
+        sql = ("select id, "
+               "       name "
+               "  from tbl_stu "
+               "  join tbl_stu_crs "
+               "    on tbl_stu.id = tbl_stu_crs.stu_id "
+               "  join tbl_stu_crs_grd "
+               "    on a = b "
+               "   and c = d "
+               " where stu_sem = '2020-Sp' "
+               "   and enrl = 1;")
+
+        query = Query(sql)
+
+        # print(dir(sql_tokens[-1]))
+        # print(sql_tokens[-1].tokens)
+
 
 if __name__ == '__main__':
     unittest.main()
