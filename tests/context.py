1680eb1d187ed2d52d57db07f16a854b67b715bf
Bryce Caine <brycecaine@gmail.com>
2020-09-12 20:17:18 -0700

    refactor: remove unneeded code

--- tests/context.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-
-from sqlpt.api import (
-    extract_from_clause, extract_where_clause, tokenize)  # , fused)
-from sqlpt.query import (
-    Query, Join, Table, FromClause, WhereClause, Field, Comparison)
