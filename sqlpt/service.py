121944b736427601b25aefd97f650960344c40e5
Bryce Caine <brycecaine@gmail.com>
2021-06-20 01:15:41 -0700

    refactor(sql): move more methods to service.py

--- sqlpt/service.py
+++ sqlpt/service.py
@@ -1,5 +1,9 @@
+from sqlparse.sql import Comparison as SQLParseComparison
 import re
+
 import sqlparse
+from sqlparse.sql import (Function, Identifier, IdentifierList, Parenthesis,
+                          Token)
 
 
 def remove_whitespace(token_list):
@@ -30,3 +34,151 @@ def get_function_from_statement(statement):
     function_str = match.group() if match else ''
 
     return function_str
+
+def get_field_expression(field_str):
+    field_expression = ''
+
+    if field_str not in (',', ' ', '\n'):
+        function_str = get_function_from_statement(field_str)
+
+        if function_str:
+            field_expression = function_str
+
+        else:
+            field_statement_elements = field_str.rsplit(' ', 1)
+            field_expression = field_statement_elements[0]
+
+    return field_expression
+
+
+def get_field_alias(field_str):
+    field_alias = ''
+
+    if field_str not in (',', ' ', '\n'):
+        function_str = get_function_from_statement(field_str)
+
+        if function_str:
+            field_alias = (field_str.replace(function_str, '')
+                                    .replace(' as ', '')
+                                    .replace(' AS ', '')
+                                    .replace(' ', ''))
+
+        else:
+            field_statement_elements = field_str.rsplit(' ', 1)
+
+            if len(field_statement_elements) > 1:
+                field_alias = field_statement_elements[1]
+
+            else:
+                field_alias = ''
+
+    return field_alias
+
+
+def is_select(item):
+    item_is_select = False
+
+    if type(item) == Token and 'select' in str(item).lower():
+        item_is_select = True
+
+    return item_is_select
+
+
+def get_field_strs(select_clause_str):
+    field_strs = []
+
+    sql_elements = sqlparse.parse(select_clause_str)
+
+    sql_tokens = remove_whitespace(sql_elements[0].tokens)
+
+    select_fields = None
+
+    for i, item in enumerate(sql_tokens):
+        if is_select(item):
+            select_fields = sql_tokens[i+1]
+
+    if type(select_fields) == IdentifierList:
+        for identifier in select_fields:
+            field_str = str(identifier)
+
+            if field_str:
+                field_strs.append(field_str)
+
+    elif type(select_fields) in (Identifier, Function, Token):
+        identifier = select_fields
+        field_str = str(identifier)
+
+        if field_str:
+            field_strs.append(field_str)
+
+    return field_strs
+
+
+def is_equivalent(object_list_1, object_list_2):
+    # Check if all of list 1 is equivalent with members of list 2
+    list_1_equivalence = {}
+
+    for list_1_object in object_list_1:
+        list_1_equivalence[list_1_object] = False
+
+        for list_2_object in object_list_2:
+            if list_1_object.is_equivalent_to(list_2_object):
+                list_1_equivalence[list_1_object] = True
+                break
+
+    list_1_equivalent = all(list_1_equivalence.values())
+
+    # Check if all of list 2 is equivalent with members of list 1
+    list_2_equivalence = {}
+
+    for list_2_object in object_list_2:
+        list_2_equivalence[list_2_object] = False
+
+        for list_1_object in object_list_1:
+            if list_2_object.is_equivalent_to(list_1_object):
+                list_2_equivalence[list_2_object] = True
+                break
+
+    list_2_equivalent = all(list_2_equivalence.values())
+
+    equivalent = list_1_equivalent and list_2_equivalent
+
+    return equivalent
+
+
+def is_join(item):
+    item_is_join = False
+
+    if type(item) == Token and 'join' in str(item).lower():
+        item_is_join = True
+
+    return item_is_join
+
+
+def is_dataset(item):
+    item_is_dataset = False
+
+    if type(item) in (Identifier, Parenthesis):
+        item_is_dataset = True
+
+    return item_is_dataset
+
+
+def is_conjunction(item):
+    item_is_conjunction = False
+
+    item_str = str(item).lower()
+
+    if type(item) == Token and ('on' in item_str or 'and' in item_str):
+        item_is_conjunction = True
+
+    return item_is_conjunction
+
+
+def is_comparison(item):
+    item_is_comparison = False
+
+    if type(item) == SQLParseComparison:
+        item_is_comparison = True
+
+    return item_is_comparison
--- sqlpt/sql.py
+++ sqlpt/sql.py
@@ -1,13 +1,15 @@
-from sqlite3 import connect
 from copy import deepcopy
 from dataclasses import dataclass
+from sqlite3 import connect
 
 import pandas as pd
 import sqlparse
 from sqlalchemy import create_engine
 from sqlparse.sql import Comparison as SQLParseComparison
-from sqlparse.sql import Identifier, IdentifierList, Parenthesis, Function, Token, Where
-from sqlpt.service import remove_whitespace, get_function_from_statement
+from sqlparse.sql import Identifier, Parenthesis, Token, Where
+
+from sqlpt.service import (get_field_alias, get_field_expression,
+                           is_join, is_dataset, is_comparison, get_field_strs, is_equivalent, remove_whitespace)
 
 
 @dataclass
@@ -34,46 +36,6 @@ class Table(DataSet):
         return equivalent
 
 
-def get_field_expression(field_str):
-    field_expression = ''
-
-    if field_str not in (',', ' ', '\n'):
-        function_str = get_function_from_statement(field_str)
-
-        if function_str:
-            field_expression = function_str
-
-        else:
-            field_statement_elements = field_str.rsplit(' ', 1)
-            field_expression = field_statement_elements[0]
-
-    return field_expression
-
-
-def get_field_alias(field_str):
-    field_alias = ''
-
-    if field_str not in (',', ' ', '\n'):
-        function_str = get_function_from_statement(field_str)
-
-        if function_str:
-            field_alias = (field_str.replace(function_str, '')
-                                    .replace(' as ', '')
-                                    .replace(' AS ', '')
-                                    .replace(' ', ''))
-
-        else:
-            field_statement_elements = field_str.rsplit(' ', 1)
-
-            if len(field_statement_elements) > 1:
-                field_alias = field_statement_elements[1]
-
-            else:
-                field_alias = ''
-
-    return field_alias
-
-
 @dataclass
 class Field:
     field_str: str
@@ -95,36 +57,6 @@ class Field:
         return description
 
 
-def get_fields(select_clause_str):
-    fields = []
-
-    sql_elements = sqlparse.parse(select_clause_str)
-
-    sql_tokens = remove_whitespace(sql_elements[0].tokens)
-
-    select_fields = None
-
-    for i, item in enumerate(sql_tokens):
-        if is_select(item):
-            select_fields = sql_tokens[i+1]
-
-    if type(select_fields) == IdentifierList:
-        for identifier in select_fields:
-            field = Field(str(identifier))
-
-            if field:
-                fields.append(field)
-
-    elif type(select_fields) in (Identifier, Function, Token):
-        identifier = select_fields
-        field = Field(str(identifier))
-
-        if field:
-            fields.append(field)
-
-    return fields
-
-
 @dataclass
 class SelectClause:
     fields: list
@@ -133,7 +65,13 @@ class SelectClause:
         return hash(str(self))
 
     def __init__(self, select_clause_str):
-        self.fields = get_fields(select_clause_str)
+        field_strs = get_field_strs(select_clause_str)
+
+        # TODO: Convert to list comprehension (multi-line)
+        self.fields = []
+
+        for field_str in field_strs:
+            self.fields.append(Field(field_str))
 
     def __str__(self):
         select_clause_str = ''
@@ -192,38 +130,6 @@ class Join:
         return equivalent
 
 
-def is_equivalent(object_list_1, object_list_2):
-    # Check if all of list 1 is equivalent with members of list 2
-    list_1_equivalence = {}
-
-    for list_1_object in object_list_1:
-        list_1_equivalence[list_1_object] = False
-
-        for list_2_object in object_list_2:
-            if list_1_object.is_equivalent_to(list_2_object):
-                list_1_equivalence[list_1_object] = True
-                break
-
-    list_1_equivalent = all(list_1_equivalence.values())
-
-    # Check if all of list 2 is equivalent with members of list 1
-    list_2_equivalence = {}
-
-    for list_2_object in object_list_2:
-        list_2_equivalence[list_2_object] = False
-
-        for list_1_object in object_list_1:
-            if list_2_object.is_equivalent_to(list_1_object):
-                list_2_equivalence[list_2_object] = True
-                break
-
-    list_2_equivalent = all(list_2_equivalence.values())
-
-    equivalent = list_1_equivalent and list_2_equivalent
-
-    return equivalent
-
-
 @dataclass
 class FromClause:
     from_table: Table
@@ -253,7 +159,7 @@ class FromClause:
 
 @dataclass
 class Comparison:
-    # Figure out a new name for "expression" (maybe "value"?)
+    # TODO: Figure out a new name for "expression" (maybe "value"?)
     left_expression: str
     operator: str
     right_expression: str
@@ -333,53 +239,6 @@ class WhereClause:
                     pass
 
         return self
-
-
-def is_select(item):
-    item_is_select = False
-
-    if type(item) == Token and 'select' in str(item).lower():
-        item_is_select = True
-
-    return item_is_select
-
-
-def is_join(item):
-    item_is_join = False
-
-    if type(item) == Token and 'join' in str(item).lower():
-        item_is_join = True
-
-    return item_is_join
-
-
-def is_dataset(item):
-    item_is_dataset = False
-
-    if type(item) in (Identifier, Parenthesis):
-        item_is_dataset = True
-
-    return item_is_dataset
-
-
-def is_conjunction(item):
-    item_is_conjunction = False
-
-    item_str = str(item).lower()
-
-    if type(item) == Token and ('on' in item_str or 'and' in item_str):
-        item_is_conjunction = True
-
-    return item_is_conjunction
-
-
-def is_comparison(item):
-    item_is_comparison = False
-
-    if type(item) == SQLParseComparison:
-        item_is_comparison = True
-
-    return item_is_comparison
 
 
 def get_join_kind(item):
