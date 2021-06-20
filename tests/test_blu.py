2c7b148582dcd85e62bed45e47f162221d9806d4
Bryce Caine <brycecaine@gmail.com>
2021-06-19 22:02:04 -0700

    fix(tests): fix tests

--- tests/test_blu.py
+++ tests/test_blu.py
@@ -25,24 +25,30 @@ class FunctionalTestCase(TestCase):
         population_sql_str = remove_whitespace_from_str(
             self.population_sql_str)
 
-        print('vvvvvvvvvvvvvvvvvvvv')
-        print(value_sql_str)
         self.value_query = Query(value_sql_str)
-        print(self.value_query)
-        print(self.value_query.select_clause)
-        print(self.value_query.from_clause)
         self.population_query = Query(population_sql_str)
 
     def test_logic_unit(self):
-        print('uuuuuuuuuuuuuuuuuuuuu')
-        print(self.value_query)
         lu = LogicUnit(name='Registered Student',
                        query=self.value_query,
                        db_source='college.db')
-        print(lu.get_value(student_id=1))
+        
+        actual_result = lu.get_value(student_id=1)
+        expected_result = 1
+
+        self.assertEqual(actual_result, expected_result)
 
     def test_population_of_logic_unit(self):
         lu = LogicUnit(name='Registered Student',
                        query=self.population_query,
                        db_source='college.db')
-        print(lu.get_population())
+
+        actual_result = lu.get_population()
+        expected_result = [
+            (1, 1, '1', 1, 'MATH'),
+            (2, 1, '2', 1, 'MATH'),
+            (3, 2, '1', 1, 'ENGL'),
+            (4, 2, '2', 1, 'ENGL'),
+        ]
+
+        self.assertEqual(actual_result, expected_result)
