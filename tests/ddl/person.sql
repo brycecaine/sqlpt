455731b4c6470370b253d9a70695007feadec97e
Bryce Caine <brycecaine@gmail.com>
2021-12-28 17:18:13 -0800

    fix(sqlpt): update databases

--- tests/ddl/person.sql
+++ tests/ddl/person.sql
@@ -2,8 +2,10 @@ create table person (
     id int primary key not null,
     id_number varchar not null,
     name varchar not null,
-    birth_date date not null
+    birth_date date not null,
+    favorite_food varchar,
+    shoe_size number
 );
 
-insert into person values (1, '123456', 'Bob Bobson', '2001-01-01');
-insert into person values (2, '123457', 'Jane Janeson', '2002-02-02');
+insert into person values (1, '123456', 'Bob Bobson', '2001-01-01', 'lasagna', '11');
+insert into person values (2, '123457', 'Jane Janeson', '2002-02-02', 'pad thai', '9');
